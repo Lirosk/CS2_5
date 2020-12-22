0e523328cbed401b39c51f45be499f430c88de5a
Lirosk <62136946+Lirosk@users.noreply.github.com>
2020-12-22 14:45:50 +0300

    Add files via upload

---
+++ DataAccess/DBAccess.cs
@@ -0,0 +1,42 @@
+﻿using System;using System.Collections.Generic;using System.Data.SqlClient;using System.Threading.Tasks;using Extensions;using Parsers;using System.Reflection;
+using Models;
+using Models.Result;
+
+namespace DataAccess{	public class DBAccess	{		public readonly SqlConnection connection;		public Task<Result<T>> GetTable<T>(string commandPath) where T : new()		{			return Task.Run(() =>			{
+				try
+				{
+					connection.Open();
+
+					var preSqlCommand = Parser.LazyParse<PreSqlCommand>(commandPath);
+					SqlCommand command = preSqlCommand.ToSqlCommand();
+					command.Connection = connection;
+
+					var assembly = typeof(Order).Assembly;
+
+					var type = assembly.GetType(preSqlCommand.Model);
+
+					if (type is null)
+					{
+						type = assembly.GetType("Models." + preSqlCommand.Model, true);
+					}
+
+					MethodInfo execute = typeof(SqlCommandExtensions).GetMethod("Execute", BindingFlags.Public | BindingFlags.Static);
+					execute = execute.MakeGenericMethod(type);
+
+
+					var res = new Result<T>()
+					{
+						Table = execute.Invoke(null, new object[] { command }) as IEnumerable<T>,
+						TypeOfTable = type
+					};
+
+					return res;
+				}
+				catch (Exception ex)
+				{
+					throw ex;
+				}
+				finally
+				{
+					connection.Close();
+				}			});		}		public DBAccess(SqlConnection connection)		{			this.connection = connection;		}		public DBAccess(string connectionString)		{			connection = new SqlConnection(connectionString);		}		public DBAccess() { }	}}
---
+++ DataAccess/Extensions/SqlCommandExtensions.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+	//with help from Artsiom
+	public static class SqlCommandExtensions
+	{
+		public static IEnumerable<TEntity> Execute<TEntity>(this SqlCommand command) where TEntity : new()
+		{
+			var result = ExecuteInternal<TEntity>(command);
+			return result;
+		}
+
+		private static IEnumerable<TEntity> ExecuteInternal<TEntity>(SqlCommand command) where TEntity : new()
+		{
+			var reader = command.ExecuteReader();
+
+			if (!reader.HasRows)
+			{
+				reader.Close();
+				return Enumerable.Empty<TEntity>();
+			}
+
+			reader.ParseFromReaderInternal<TEntity>().Wait();
+			var entities = reader.ParseFromReaderInternal<TEntity>().Result;
+			reader.Close();
+
+			return entities;
+		}
+
+		private static Task<IEnumerable<TEntity>> ParseFromReaderInternal<TEntity>(this SqlDataReader reader) where TEntity : new()
+		{
+			return Task.Run(() =>
+			{
+				var entityType = typeof(TEntity);
+				var entityProps = entityType.GetProperties();
+
+				var entities = new List<TEntity>();
+
+				TEntity entity;
+				object valueFromReader;
+
+				while (reader.Read())
+				{
+					entity = new TEntity();
+					foreach (var entityPropInfo in entityProps)
+					{
+						try
+						{
+							valueFromReader = reader[entityPropInfo.Name];
+							if (valueFromReader is DBNull)
+							{
+								valueFromReader = null;
+								continue;
+							}
+
+							entityPropInfo.SetValue(entity, valueFromReader);
+						}
+						catch (IndexOutOfRangeException)
+						{
+							continue;
+						}
+					}
+
+					entities.Add(entity);
+				}
+				return entities as IEnumerable<TEntity>;
+			});
+		}
+	}
+}
